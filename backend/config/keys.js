ec2d44ab9c19a6323aacc3fd43b3a2c9def3506a
Rye Cassan <26152348+hondarider29@users.noreply.github.com>
2021-04-15 14:39:21 -0500

    Basic DB Connection

---
+++ backend/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://cloudHavenDev:vklxZ5iyNtJmwY9m@cloudhaven.ngvn7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+}
