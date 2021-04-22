3df9dfcbcb104bcaa7041ed745986a1fb9967747
Joe Soboleski <soboleskijoe@gmail.com>
2021-04-22 10:23:28 -0700

    upgraded component to ES6 notation

--- frontend/src/Pages/LandingPage/LandingPage.jsx
+++ frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -1,28 +1,28 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
+import React from 'react'
+import { withRouter } from 'react-router-dom'
 import {
-    Background,
-    Title,
-    SubText,
-    ButtonRow,
-    Button,
-} from "./LandingPage.styled";
+  Background,
+  Title,
+  SubText,
+  ButtonRow,
+  Button
+} from './LandingPage.styled'
 
-function LandingPage() {
-    return (
-        <Background>
-            <Title>Welcome Home</Title>
-            <SubText>It's CLOUDHAVEN!</SubText>
-            <ButtonRow>
-                <Button filled={false} to="/info">
-                    How it Works
-                </Button>
-                <Button filled to="/login">
-                    Get Started
-                </Button>
-            </ButtonRow>
-        </Background>
-    );
+const LandingPage = () => {
+  return (
+    <Background>
+      <Title>Welcome Home</Title>
+      <SubText>It's CLOUDHAVEN!</SubText>
+      <ButtonRow>
+        <Button filled={false} to='/info'>
+          How it Works
+        </Button>
+        <Button filled to='/login'>
+          Get Started
+        </Button>
+      </ButtonRow>
+    </Background>
+  )
 }
 
-export default withRouter(LandingPage);
+export default withRouter(LandingPage)
