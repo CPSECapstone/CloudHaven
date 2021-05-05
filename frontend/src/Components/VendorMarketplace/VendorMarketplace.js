6d418fbc71ece0b84d36c811083a2fc317dd57ce
Adrien Chaussabel <adrien.chaussabel@gmail.com>
2021-05-05 12:03:04 -0700

    Converted marketplace component into functional component

--- frontend/src/Components/VendorMarketplace/VendorMarketplace.js
+++ frontend/src/Components/VendorMarketplace/VendorMarketplace.js
@@ -1,45 +1,25 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import SearchBar from './SearchBar.js';
 import Content from './Content.js';
 import { SideBar, TopBar } from '../components';
 
+const VendorMarketplace = () => {
+  const [search, setSearch] = useState('');
 
-/**
- * VendorMarketplace component. 
- */
-
-//  * NEEDS TO BE MADE INTO FUNCTIONAL COMPONENT
-class Homepage extends Component {
-  /**
-   * @param {*} props - not used
-   */
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: '',
-    };
-  }
-
-  /**
-   * Renders vendor marketplace
-   * @return {div} - Returns the marketplace components
-   */
-  render() {
-    return (
-      <div>
-        <div id="HomePage">
-          <SideBar />
-          <div id="PageFrame">
-              <TopBar />
-              <div id="MainContent">
-              <SearchBar onChange={(value) => this.setState({search: value})}/>
-              <Content searchInput={this.state.search}/>
-              </div>
-          </div>
+  return (
+    <div>
+      <div id="HomePage">
+        <SideBar />
+        <div id="PageFrame">
+            <TopBar />
+            <div id="MainContent">
+            <SearchBar onChange={(value) => setSearch(value)}/>
+            <Content searchInput={search}/>
+            </div>
         </div>
       </div>
-    );
-  }
-};
+    </div>
+  );
+}
 
-export default Homepage;
+export default VendorMarketplace;
