a0f0ebeede2a15ab37b1808eb0174a07e6949617
Nathan Palmiero <ncpalmie@calpoly.edu>
2021-05-05 17:11:36 -0400

    Added dynamic vendor rendering based on user subscribed services

--- frontend/src/Components/SideBar/SideBar.js
+++ frontend/src/Components/SideBar/SideBar.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './SideBar.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faBars, faHome, faPlus, faQuestion} from "@fortawesome/free-solid-svg-icons";
+import axios from 'axios';
 
 const homeIcon = <FontAwesomeIcon icon={faHome} />
 const barIcon = <FontAwesomeIcon icon={faBars}/>
 const questionMarkIcon = <FontAwesomeIcon icon={faQuestion}/>
 const plusIcon = <FontAwesomeIcon icon={faPlus} />
 
-const menuListItemsConfig = [
+const baseMenuConfig = [
     {iconPath: homeIcon, urlPath: "/", listID: "Home"},
-    {iconPath: questionMarkIcon, urlPath: "/Test", listID: "TestApplication"},
     {iconPath: plusIcon, urlPath: "/market", listID: "VendorMarket"}
 ];
 
@@ -38,15 +38,40 @@ const renderSidebarMenuOption = (listID, iconPath, urlPath, selected) => {
 
 const SideBar = () => {
     const menuItems = [];
-    const currentPath = window.location.pathname; 
+    const currentPath = window.location.pathname;
+    const [menuListItemsConfig, setMenuListItemsConfig] = useState(baseMenuConfig);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const showSidebar = () => setIsSidebarOpen(!isSidebarOpen);
+    const className = isSidebarOpen ? "SideBar SideBarOpen" : "SideBar SideBarClosed";
+
+    useEffect(() => {
+        const fetchUserVendors = async () => {
+            try {
+                const response = await axios('/users/vendors');
+                const newMenu = [
+                    {iconPath: homeIcon, urlPath: "/", listID: "Home"},
+                    {iconPath: plusIcon, urlPath: "/market", listID: "VendorMarket"}
+                ];
+    
+                for (const item of response.data) {
+                    const config = {iconPath: questionMarkIcon, urlPath: item.home_route, listID: item.name}
+                    newMenu.splice(1, 0, config)
+                }
+         
+                setMenuListItemsConfig(newMenu);
+            } catch (err) {
+                console.log(err + ' | Failed to get user subscribed vendors');
+            }
+        };
+     
+        fetchUserVendors();
+
+    }, []);
+    
 
     for (const itemConfig of menuListItemsConfig){
         menuItems.push(renderSidebarMenuOption(itemConfig.listID, itemConfig.iconPath, itemConfig.urlPath, currentPath===itemConfig.urlPath));
     }
-
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const showSidebar = () => setIsSidebarOpen(!isSidebarOpen);
-    const className = isSidebarOpen ? "SideBar SideBarOpen" : "SideBar SideBarClosed";
 
     return(
         <div className={className}>
