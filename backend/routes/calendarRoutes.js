e46d008692851e7cceddc67648569214ba59fb9c
Rye Cassan <26152348+hondarider29@users.noreply.github.com>
2021-05-05 19:48:53 -0500

    calendarRoutes essential routes

--- backend/routes/calendarRoutes.js
+++ backend/routes/calendarRoutes.js
@@ -53,13 +53,15 @@ router.get('/calendar/init', function(req, res){
       color: 0x330099
    });
    newEvent6.save();
+
+   res.send("Test Events added to Database");
 });
 
 // Grab All Events By User
-router.get('/calendar/:_id', function(req, res) {
-   eventModel.find({user: req.parm._id}, (function(err, eventData) {
+router.get('/calendar/:user', function(req, res) {
+   eventModel.find({user: req.parm.user}, (function(err, eventData) {
       if (err || eventData === null) {
-         res.send("User " + req.parm._id + " does not have any events");
+         res.send("User " + req.parm.user + " does not have any events");
       } else {
          const userEvents = eventData.map((eventDoc) => {
             return eventDoc.toObject();
@@ -68,4 +70,60 @@ router.get('/calendar/:_id', function(req, res) {
       }
    })
    );
+});
+
+// Grab All Events By User and Vendor
+router.get('/calendar/:user/:vendor', function(req, res) {
+   eventModel.find({user: req.parm.user, vendor: req.parm.vendor}, (function(err, eventData) {
+      if (err || eventData === null) {
+         res.send("User " + req.parm.user + " does not have any events assciated with Vendor " + req.parm.vendor);
+      } else {
+         const userEvents = eventData.map((eventDoc) => {
+            return eventDoc.toObject();
+         })
+         res.send(userEvents);
+      }
+   })
+   );
+});
+
+// Add, Edit, and Delete Events By User
+router.post('/calendar/:user', function(req, res) {
+   var data = req.body;
+
+   var mode = data["!nativeeditor_status"];
+   
+   var sid = data.id;
+   var tid = sid;
+
+   delete data.id;
+   delete data["!nativeeditor_status"];
+
+   function update_response(err, result) {
+      if (err) {
+         mode = 'error';
+      } else if (mode == "inserted") {
+         tid = data._id;
+      }
+      res.setHeader("Content-Type", "application/json");
+      res.send({action: mode, sid: sid, tid: tid});
+   }
+
+   if (mode == "updated") {
+      eventModel.updateOne(sid, data, update_response);
+   } else if (mode == "inserted") {
+      const newEvent = new eventModel({
+         user: data.user,
+         vendor: data.vendor,
+         text: data.text,
+         start_date: data.start_date,
+         end_date: data.end_date,
+         color: data.color
+      });
+      newEvent.save(update_response);
+   } else if (mode == "deleted") {
+      eventModel.deleteOne(sid, update_response);
+   } else {
+      res.send("Unsupported Operation");
+   }
 });
