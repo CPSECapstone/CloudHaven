2572a40a514580f3bfc9e6b6c3ecbbb6194e256e
terence <tjtong2006@gmail.com>
2021-05-17 18:19:55 -0700

    temporary VendorAppPage

--- frontend/src/Components/VendorApp/VendorApp.js
+++ frontend/src/Components/VendorApp/VendorApp.js
@@ -3,8 +3,138 @@ import React from "react";
 import { TabSystem } from "../components";
 import "./VendorApp.css";
 
-export default (props) => {
+var testVendorAppData = {
+    VendorId: "AH",
+    VendorName: "Apollo Healthcare",
+    VendorAuth: "Password",
+    Components: [
+        {
+            Component: 'file',
+            id: 0,
+            title: 'New Patient Wavier',
+            datePosted: '11/12/20',
+            dueDate: '12/31/20',
+            fileType: 'pdf',
+            filePath: 0,
+        },
+        {
+            Component: 'file',
+            id: 1,
+            title: 'Health Insurance',
+            datePosted: '11/12/20',
+            dueDate: '12/09/20',
+            fileType: 'pdf',
+            filePath: 1,
+        },
+        {
+            Component: 'file',
+            id: 2,
+            title: 'New Patient Waiver',
+            datePosted: '11/11/20',
+            dueDate: '12/9/20',
+            fileType: 'form',
+            Description: "Some description",
+            Fields: [
+                {
+                    Child: "email",
+                    Match: "regex pattern",
+                    label: "Email",
+                    Validate: true,  // optional
+                    Required: false // optional
+                },
+                {
+                    Child: "phoneNumber",
+                    label: "Phone number",
+                    Validate: false,
+                    Required: true
+                },
+                {
+                    Child: "date",
+                    label: "Birthday",
+                    Validate: true,
+                    Required: true
+                },
+                {
+                    Child: "textbox",
+                    label: "Driver’s license number",
+                    Validate: false,
+                    Required: true
+                },
+            ],
+        },
+        {
+            Component: "card",
+            Header: "header text",
+            body: "inner text",
+            Footer: "footer text"
+        },
+        {
+            Component: "button",
+            Text: "inner text",
+            "Submit location": "url"
+            //Callback: STRETCH GOAL
+        },
+        {
+            Component: "link",
+            Src: "url",
+            Alt: "alt text"
+        },
+        {
+            Component: "image",
+            Src: "url",
+            Alt: "alt text"
+        },
+        {
+            Component: "text",
+            Content: "I am text",
+            Label: "text label"  // optional
+        },
+        {
+            Component: "table",
+            display: 5,
+            headers: [    // optional
+                "Header 1"
+            ],
+            rows: [
+                {
+                    Component: "text",
+                    Content: "I am a table cell",
+                },
+                {
+                    Component: "image",
+                    Src: "url",
+                    Alt: "table image"
+                }
+            ]
+        }
+    ]
+};
+
+
+const VendorApp = () => {
+    useEffect(() => {
+        getVendorAppData();
+    }, []);
+
+    const [vendorId, setVendorId] = useState("");
+
+    const [vendorAuth, setVendorAuth] = useState("");
+
+    const [components, setComponents] = useState([]);
+
+    const getVendorAppData = () => {
+        setVendorId(testVendorAppData.VendorId);
+        setVendorAuth(testVendorAppData.VendorAuth);
+        setComponents(testVendorAppData.Components);
+    };
+
+    if (components.length == 0) {
+        return null;
+    }
+
     return (
-        <TabSystem dynamicTabIcon={vendorAppIcon} dynamicTabTitle="Vendor App"/>
+        <VendorAppPage vendorId={vendorId} components={components} />
     )
-} 
+}
+
+export default VendorApp;
---
+++ frontend/src/Components/VendorAppPage/VendorAppPage.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { FileList, CustomForm, FileViewer } from '../components'
+import './VendorAppPage.css'
+import pdfTest1 from '../../Utils/test1.pdf'
+import pdfTest2 from '../../Utils/test2.pdf'
+
+const isOnVendorHomePage = false
+
+// NOTE: until we figure out how we want to handle accessing pdfs from the backend,
+// we have these temporary test pdf files here. This is definitely a temporary measure.
+// Right now, we just get an index number from the backend and use that number to choose
+// from the two below test files.
+const testPdfs = [
+    pdfTest1,
+    pdfTest2,
+];
+
+export default (props) => {
+    const files = props.components.filter(component => component.Component === 'file');
+    const [currentFileIndex, setCurrentFileIndex] = React.useState(0);
+    var currentFile = files[currentFileIndex];
+
+    return (
+        <React.Fragment>
+            {isOnVendorHomePage && <h1>{props.vendorId}</h1>}
+            <div id="FileFeaturePage">
+                <FileList
+                    files={files}
+                    setCurrentFile={setCurrentFileIndex}
+                    currentFileId={currentFile.id}
+                />
+                {currentFile.fileType == 'form' ? (
+                    <CustomForm name={currentFile.title} fields={currentFile.Fields} />
+                ) : (
+                    <FileViewer filePath={testPdfs[currentFile.filePath]} />
+                )}
+            </div>
+        </React.Fragment>
+    )
+}
