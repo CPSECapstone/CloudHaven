722d7db1b6067277c6bb1cbf36a082bb03945ce9
Rye Cassan <26152348+hondarider29@users.noreply.github.com>
2021-05-20 13:44:46 -0500

    Created Chat Schema

---
+++ backend/models/Chats.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ChatSchema = new Schema({
+   sendor: {
+      // User's ObjectId repersented as a String
+      type: String,
+      required: true
+   },
+   reciever: {
+      // User's ObjectId repersented as a String
+      type: String,
+      required: true
+   },
+   vendor: {
+      // Vendor's ObejctID repersented as a String
+      type: String,
+      default: null
+   },
+   message: {
+      type: String,
+      default: ""
+   },
+   timeStamp: {
+      type: Date,
+      required: true
+   },
+   color: {
+      type: Number
+   }
+});
+
+module.exports = ChatMessage = mongoose.model("ChatMessages", EventSchema);
