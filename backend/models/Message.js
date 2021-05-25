7d544964bc59ecd61beed13069a030462f8247ed
Rye Cassan <26152348+hondarider29@users.noreply.github.com>
2021-05-25 16:05:03 -0500

    Added a MessageSchema and incorpeated it

---
+++ backend/models/Message.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+   sendor: {
+      // User's ObjectId repersented as a String
+      type: String,
+      required: true
+   },
+   timeStamp: {
+      type: Date,
+      required: true
+   },
+   text: {
+      type: String,
+      default: ""
+   }
+});
+
+module.exports = Messages = mongoose.model("Messages", MessageSchema);
