226612093e05f10b7fec44341341a876fa393246
David Sann <davidsann92@gmail.com>
2021-06-01 15:39:13 -0700

    Fixed offsetHeightError

--- frontend/src/Components/Calendar/Calendar.jsx
+++ frontend/src/Components/Calendar/Calendar.jsx
@@ -2,14 +2,16 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import Calendar from 'react-awesome-calendar';
 import { Frame } from "./Calendar.styled";
+import axios from 'axios';
 
 function MyCalendar() {
 
   const [allEvents, setEvents] = useState([]);
+  let events = [];
 
   useEffect(() => {
-    //fetchEvents();
     fetchUser();
+    fetchEvents();
   }, [])
 
   /*
@@ -22,23 +24,24 @@ function MyCalendar() {
   }
   */  
 
-  const fetchUser = () => {
-    fetch("http://localhost:4000//users/all")
-      .then(res => res.text())
-      .then(res => console.log(res))
-      .catch(err => console.err(err))
+  const fetchEvents = async () => {
+    events.push({
+      id: 1,
+      color: '#3694DF',
+      from: '2021-06-05T13:00:00+00:00',
+      to: '2021-06-05T20:00:00+00:00',
+      title: "new EVENT"
+    });
   }
 
-  let events = [];
-
-  // Example Event
-  events.push({
-    id: 1,
-    color: '#3694DF',
-    from: '2021-07-05T13:00:00+00:00',
-    to: '2021-07-05T20:00:00+00:00',
-    title: "new EVENT"
-  });
+  const fetchUser = async () => {
+    try {
+        const response = await axios('/users/all');
+        console.log(response.data)
+    } catch (err) {
+        console.log(err + ' | Failed to get user data');
+    }
+  };
   
   return (
     <Frame>
