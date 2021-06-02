a69039eff763dbf3e360b24a4575791fa130ad13
terence <tjtong2006@gmail.com>
2021-06-01 17:00:39 -0700

    new routes created needs testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,43 @@
 import { Home, Login, Registration, _404 } from './Components/components';
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import {BrowserRouter, Redirect, Switch} from 'react-router-dom';
 import PrivateRoute from './CustomRoutes/PrivateRoute';
 import PublicRoute from './CustomRoutes/PublicRoute';
 import LandingPage from './Components/LandingPage/LandingPage'
 import VendorMarketplace from './Components/VendorMarketplace/VendorMarketplace';
+import axios from 'axios';
 
-function App () {
+const App = (props) => {
+  const [vendorRoutes, setVendorRoutes] = useState([]);
+
+  useEffect(() => {
+    const getVendorRoutes = async () => {
+      try {
+        const response = await axios('/vendors');
+        for (const item of response.data) {
+          setVendorRoutes(vendorRoutes => [...vendorRoutes, item.home_route])
+        }
+      } catch (err) {
+        console.log(err + ' | Failed to get all vendors');
+      }
+    };
+    getVendorRoutes();
+
+    if (props.setUpdateApp) {
+      props.setUpdateApp(false);
+    }
+  }, [props.updateApp]);
+
+  const populateRoutes = () => {
+    console.log(vendorRoutes);
+    return (vendorRoutes).map((pathName) => {
+      return (
+        <PublicRoute restricted={false} path={pathName} component = {VendorMarketplace}/>
+      );
+    })
+  }
+
   return (
     <BrowserRouter>
         <Switch>
@@ -17,6 +47,7 @@ function App () {
           <PublicRoute restricted={true} path='/register'
             component={Registration}/>
           <PublicRoute restricted={false} path='/market' component={VendorMarketplace}/>
+          {populateRoutes()}
           <PublicRoute restricted={false} path='/404' component = {_404}/>
           <Redirect to='/404' />
         </Switch>
