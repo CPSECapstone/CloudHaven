053a14eb35870e9874fa4e6e7292b2676a01fcb9
David Sann <davidsann92@gmail.com>
2021-06-02 19:14:06 -0700

    Added an AddEvent Function

--- frontend/src/Components/Calendar/Calendar.jsx
+++ frontend/src/Components/Calendar/Calendar.jsx
@@ -17,24 +17,52 @@ function MyCalendar() {
   const [allEvents, setEvents] = useState([]);
   let events = [];
 
+  useEffect(async () => {
+    try {
+      const response = await axios('/users/all');
+      setUser(response.data._id);
+    } catch (err) {
+      console.log(err + ' | Failed to get user ID');
+    }
+  }, [])
+
   useEffect(() => {
     fetchUserEvents();
-  }, [])
+  }, [userId])
 
   useEffect(() => {
     createEvents();
   }, [allEvents])
 
-  const fetchUserEvents= async () => {
+  const fetchUserEvents = async () => {
     try {
-        const response = await axios('/users/all');
-        const allEvents = await axios('/calendar/' + response.data._id);
+        const allEvents = await axios('/calendar/' + userId);
         setEvents(allEvents.data);
 
     } catch (err) {
         console.log(err + ' | Failed to get user events');
     }
   };
+
+  const addEvent = async (vendorId, title, start, end, color) => {
+    try {
+      const res = await axios.post('/calendar/' + userId, {
+        _id : userId,
+        user : userId,
+        vendor : vendorId,
+        text : title,
+        start_date : start,
+        end_date : end,
+        color : color
+      });
+      console.log(res);
+    } catch (err) {
+        console.log(err + ' | Failed to add new event');
+    }
+  };
+
+  // Example call to addEvent after modal submit
+  //addEvent(1, "HelloTest",'2021-06-06T19:10:00.000+00:00','2021-06-06T22:00:00.000+00:00',null);
 
   const createEvents = () => {
     for (const event of allEvents) {
@@ -55,7 +83,6 @@ function MyCalendar() {
         events = {events}
       />
     </Frame>
-      
   )
 }
 
