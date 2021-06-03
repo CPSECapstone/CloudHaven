5cf28483393a91d40531156265a7755c96379943
Rohan Ramani <rramani1230@gmail.com>
2021-06-02 20:33:08 -0700

    This creates the datepicker

--- frontend/src/Components/EventMaker/Form/index.js
+++ frontend/src/Components/EventMaker/Form/index.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import DateSelector from "../../DatePicker/DatePicker";
+
 
 export const Form = ({ onSubmit }) => {
+
   return (
     <form onSubmit={onSubmit}>
       <div className="form-group">
@@ -9,10 +12,17 @@ export const Form = ({ onSubmit }) => {
       </div>
       <div className="form-group">
         <label htmlFor="name">Starting time</label>
-        <input className="form-control" id="name" />
+        <DateSelector></DateSelector>
+        {/* <DatePicker></DatePicker> */}
+        {/* <input className="form-control" id="name" /> */}
       </div>
       <div className="form-group">
-        <label htmlFor="name">Ending Time</label>
+        <label htmlFor="name">Ending Time </label>
+        {/* <DatePicker></DatePicker> */}
+        <DateSelector showTimeSelect></DateSelector>
+      </div>
+      <div className="form-group">
+        <label htmlFor="name">Vendor Application</label>
         <input className="form-control" id="name" />
       </div>
       <div className="form-group">
